7e200c29271751970fe12a433bbdf333757cd99a
Juan David Hernandez <juandahernandez27991@gmail.com>
2024-05-27 11:05:50 -0500

    test 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import React from "react";
 import Home from "@/components/Home/Home";
-import Navigation from "@/components/Navigation/Navigation";
 
 const Indexpage = () => {
   return (
     <div>
-      <Navigation />
-
       <Home />
     </div>
   );
